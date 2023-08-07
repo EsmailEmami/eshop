c47f8ba6425d9bcb9ef42cd9fff5bc1027867cb4
Esmail Emami <esmailemami84@gmail.com>
2023-08-07 22:06:36 +0330

    add permisions consts to add permission for apis

---
+++ models/permissions.go
@@ -0,0 +1,145 @@
+package models
+
+const (
+	// ###### Product ######
+
+	ACTION_PRODUCT_ADMIN_GET = "action_product_admin_get"
+
+	// ###### Product ######
+
+	// ###### Brand ######
+
+	ACTION_BRAND_ADMIN_GET    = "action_brand_admin_get"
+	ACTION_BRAND_ADMIN_CREATE = "action_brand_admin_create"
+	ACTION_BRAND_ADMIN_LIST   = "action_brand_admin_list"
+	ACTION_BRAND_ADMIN_EDIT   = "action_brand_admin_edit"
+	ACTION_BRAND_ADMIN_DELETE = "action_brand_admin_delete"
+
+	// ###### Brand ######
+
+	// ###### Category ######
+
+	ACTION_CATEGORY_ADMIN_GET    = "action_category_admin_get"
+	ACTION_CATEGORY_ADMIN_CREATE = "action_category_admin_create"
+	ACTION_CATEGORY_ADMIN_LIST   = "action_category_admin_list"
+	ACTION_CATEGORY_ADMIN_EDIT   = "action_category_admin_edit"
+	ACTION_CATEGORY_ADMIN_DELETE = "action_category_admin_delete"
+
+	// ###### Category ######
+
+	// ###### Role ######
+
+	ACTION_ROLE_ADMIN_GET    = "action_role_admin_get"
+	ACTION_ROLE_ADMIN_CREATE = "action_role_admin_create"
+	ACTION_ROLE_ADMIN_LIST   = "action_role_admin_list"
+	ACTION_ROLE_ADMIN_EDIT   = "action_role_admin_edit"
+	ACTION_ROLE_ADMIN_DELETE = "action_role_admin_delete"
+
+	// ###### Role ######
+
+	// ###### Color ######
+
+	ACTION_COLOR_ADMIN_GET    = "action_color_admin_get"
+	ACTION_COLOR_ADMIN_CREATE = "action_color_admin_create"
+	ACTION_COLOR_ADMIN_LIST   = "action_color_admin_list"
+	ACTION_COLOR_ADMIN_EDIT   = "action_color_admin_edit"
+	ACTION_COLOR_ADMIN_DELETE = "action_color_admin_delete"
+
+	// ###### Color ######
+
+	// ###### AppPic ######
+
+	ACTION_APP_PIC_ADMIN_GET    = "action_app_pic_admin_get"
+	ACTION_APP_PIC_ADMIN_CREATE = "action_app_pic_admin_create"
+	ACTION_APP_PIC_ADMIN_LIST   = "action_app_pic_admin_list"
+	ACTION_APP_PIC_ADMIN_EDIT   = "action_app_pic_admin_edit"
+	ACTION_APP_PIC_ADMIN_DELETE = "action_app_pic_admin_delete"
+
+	// ###### AppPic ######
+
+	// ###### ProductFeatureCategory ######
+
+	ACTION_PRODUCT_FEATURE_CATEGORY_ADMIN_GET    = "action_product_feature_category_admin_get"
+	ACTION_PRODUCT_FEATURE_CATEGORY_ADMIN_CREATE = "action_product_feature_category_admin_create"
+	ACTION_PRODUCT_FEATURE_CATEGORY_ADMIN_LIST   = "action_product_feature_category_admin_list"
+	ACTION_PRODUCT_FEATURE_CATEGORY_ADMIN_EDIT   = "action_product_feature_category_admin_edit"
+	ACTION_PRODUCT_FEATURE_CATEGORY_ADMIN_DELETE = "action_product_feature_category_admin_delete"
+
+	// ###### ProductFeatureCategory ######
+
+	// ###### ProductFeatureKey ######
+
+	ACTION_PRODUCT_FEATURE_KEY_ADMIN_GET    = "action_product_feature_key_admin_get"
+	ACTION_PRODUCT_FEATURE_KEY_ADMIN_CREATE = "action_product_feature_key_admin_create"
+	ACTION_PRODUCT_FEATURE_KEY_ADMIN_LIST   = "action_product_feature_key_admin_list"
+	ACTION_PRODUCT_FEATURE_KEY_ADMIN_EDIT   = "action_product_feature_key_admin_edit"
+	ACTION_PRODUCT_FEATURE_KEY_ADMIN_DELETE = "action_product_feature_key_admin_delete"
+
+	// ###### ProductFeatureKey ######
+
+	// ###### ProductFeatureValue ######
+
+	ACTION_PRODUCT_FEATURE_VALUE_ADMIN_GET    = "action_product_feature_value_admin_get"
+	ACTION_PRODUCT_FEATURE_VALUE_ADMIN_CREATE = "action_product_feature_value_admin_create"
+	ACTION_PRODUCT_FEATURE_VALUE_ADMIN_LIST   = "action_product_feature_value_admin_list"
+	ACTION_PRODUCT_FEATURE_VALUE_ADMIN_EDIT   = "action_product_feature_value_admin_edit"
+	ACTION_PRODUCT_FEATURE_VALUE_ADMIN_DELETE = "action_product_feature_value_admin_delete"
+
+	// ###### ProductFeatureValue ######
+
+	// ###### File ######
+
+	ACTION_FILE_GET             = "action_file_get"
+	ACTION_FILE_CREATE          = "action_file_create"
+	ACTION_FILE_LIST            = "action_file_list"
+	ACTION_FILE_DELETE          = "action_file_delete"
+	ACTION_FILE_CHANGE_PRIORITY = "action_file_change_priority"
+
+	// ###### File - Systematic ######
+
+	ACTION_FILE_SYSTEMATIC_GET             = "action_file_systematic_get"
+	ACTION_FILE_SYSTEMATIC_CREATE          = "action_file_systematic_create"
+	ACTION_FILE_SYSTEMATIC_LIST            = "action_file_systematic_list"
+	ACTION_FILE_SYSTEMATIC_DELETE          = "action_file_systematic_delete"
+	ACTION_FILE_SYSTEMATIC_CHANGE_PRIORITY = "action_file_systematic_change_priority"
+
+	// ###### File - systematic ######
+
+	// ###### File - Product File Map ######
+
+	ACTION_FILE_PRODUCT_GET             = "action_file_product_get"
+	ACTION_FILE_PRODUCT_CREATE          = "action_file_product_create"
+	ACTION_FILE_PRODUCT_LIST            = "action_file_product_list"
+	ACTION_FILE_PRODUCT_DELETE          = "action_file_product_delete"
+	ACTION_FILE_PRODUCT_CHANGE_PRIORITY = "action_file_product_change_priority"
+
+	// ###### File - Product File Map ######
+
+	// ###### File - Brand ######
+
+	ACTION_FILE_BRAND_GET             = "action_file_brand_get"
+	ACTION_FILE_BRAND_CREATE          = "action_file_brand_create"
+	ACTION_FILE_BRAND_LIST            = "action_file_brand_list"
+	ACTION_FILE_BRAND_DELETE          = "action_file_brand_delete"
+	ACTION_FILE_BRAND_CHANGE_PRIORITY = "action_file_brand_change_priority"
+
+	// ###### File - Brand ######
+
+	// ###### File - AppPic ######
+
+	ACTION_FILE_APP_PIC_GET             = "action_file_app_pic_get"
+	ACTION_FILE_APP_PIC_CREATE          = "action_file_app_pic_create"
+	ACTION_FILE_APP_PIC_LIST            = "action_file_app_pic_list"
+	ACTION_FILE_APP_PIC_DELETE          = "action_file_app_pic_delete"
+	ACTION_FILE_APP_PIC_CHANGE_PRIORITY = "action_file_app_pic_change_priority"
+
+	// ###### File - AppPic ######
+
+	// ###### File ######
+
+	// ###### VerificationCode ######
+
+	ACTION_VERIFICATION_CODE_ADMIN_LIST = "action_verification_code_admin_list"
+
+	// ###### VerificationCode ######
+)
